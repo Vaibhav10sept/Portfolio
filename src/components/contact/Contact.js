ed92e488e2b2066b0d8b9dd39138c6beeedcd132
Vaibhav10sept <vaibhavtiwari10sept@gmail.com>
2020-12-27 20:31:21 +0530

    added contact modal

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,64 @@
 import React from 'react'
 import './contact.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Contact2 from '../contact2/Contact2'
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      maxWidth:"85%",
+      margin:"0 auto"
+   
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  
 function Contact() {
+    
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
     return (
         <div>
             <section id="contact-btn">
 	<h1 class="c-b-heading">If You Have Any Project In Your Mind ?</h1>
-		<button>Contact Me</button>
+		<button onClick={handleOpen}>Contact Me</button>
 	</section>
+    <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+         <Contact2/>
+        </Fade>
+      </Modal>
         </div>
     )
 }
