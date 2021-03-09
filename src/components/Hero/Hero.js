64c6c18482f19fceeb40c5d7006aeae104cd4b82
SansakrSachan <Sanskar841signh@gmail.com>
2021-03-09 21:22:10 +0530

    updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -22,7 +22,7 @@ function Hero(props) {
               Sanskar Sachan <span></span>
             </h1>
             <img src={arrow} className="arrow"/>
-              <button className="about__social-icon"><Link to="/files/VAIBHAV TIWARI(resume).pdf" target="_blank" download>Click To Download Resume</Link></button>
+              <button className="buttondownload about__social-icon"><Link to="/files/VAIBHAV TIWARI(resume).pdf" target="_blank" download>Click To Download Resume</Link></button>
           </div>
         
         </div>
